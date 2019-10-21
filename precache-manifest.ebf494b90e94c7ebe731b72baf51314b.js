082274ce4e722ea43c382b266cd257fd7ade0b4d
swarnkarneeraj <sneeraj.itech@gmail.com>
2019-10-21 21:02:08 +0530

    Updates

---
+++ precache-manifest.ebf494b90e94c7ebe731b72baf51314b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb5be8955ebb3f3ece3df8f888dced7",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "64b34807c58a6779532f",
+    "url": "/monstersrolodex/static/css/main.964328d8.chunk.css"
+  },
+  {
+    "revision": "492a1eda71f17acaeec3",
+    "url": "/monstersrolodex/static/js/2.1f8e7b39.chunk.js"
+  },
+  {
+    "revision": "64b34807c58a6779532f",
+    "url": "/monstersrolodex/static/js/main.cb7e80a7.chunk.js"
+  },
+  {
+    "revision": "4ee30963d6e5c7147114",
+    "url": "/monstersrolodex/static/js/runtime~main.6c79a31e.js"
+  }
+]);
